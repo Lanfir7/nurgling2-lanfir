bcc8d1e2c0ece6552ffd25230d7cb49c568d94af
Lanfir7 <ahapxuctuk@gmail.com>
2024-10-18 09:12:42 +0300

    Finder placeFeeler

--- release/run_updater.bat
+++ release/run_updater.bat
@@ -1,2 +1,2 @@
-java -jar nurgling_launcher.jar update https://raw.githubusercontent.com/Lanfir7/nurgling2-lanfir/master/ --add-exports=java.desktop/sun.awt=ALL-UNNAMED -Dsun.java2d.uiScale.enabled=false -jar ./hafen.jar
+java -jar nurgling_launcher.jar update https://raw.githubusercontent.com/Lanfir7/nurgling2-lanfir/master/ --add-exports=java.desktop/sun.awt=ALL-UNNAMED ./hafen.jar
 pause
--- src/nurgling/actions/StackOff.java
+++ src/nurgling/actions/StackOff.java
@@ -1,20 +1,10 @@
 package nurgling.actions;
 
-import haven.Coord;
-import haven.Inventory;
 import haven.MenuGrid;
-import haven.WItem;
-import nurgling.NGItem;
 import nurgling.NGameUI;
 import nurgling.NInventory;
 import nurgling.NUtils;
-import nurgling.tasks.WaitItemInEquip;
-import nurgling.tasks.WaitItemInHand;
-import nurgling.tools.NAlias;
-import nurgling.tools.NParser;
-import nurgling.widgets.NEquipory;
 
-import java.util.HashSet;
 
 public class StackOff {
     boolean status;
