8a546e32b30842a8fc1e1d4abe2b69e75c0857ec
Lanfir7 <ahapxuctuk@gmail.com>
2024-10-18 12:22:00 +0300

    release

--- release/run.bat
+++
@@ -1,2 +0,0 @@
-java --add-exports=java.desktop/sun.awt=ALL-UNNAMED -Dsun.java2d.uiScale.enabled=false -jar ./hafen.jar
-pause
--- release/run_updater.bat
+++
@@ -1,2 +0,0 @@
-java -jar nurgling_launcher.jar update https://raw.githubusercontent.com/Lanfir7/nurgling2-lanfir/master/ --add-exports=java.desktop/sun.awt=ALL-UNNAMED ./hafen.jar
-pause
