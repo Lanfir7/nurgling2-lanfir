91a3851869e3efce829160d47f1eef9f630ba209
Lanfir7 <ahapxuctuk@gmail.com>
2024-11-05 00:37:40 +0200

    fx

--- src/nurgling/NConfig.java
+++ src/nurgling/NConfig.java
@@ -123,7 +123,7 @@ public class NConfig
         conf.put(Key.endpoint, "");
         conf.put(Key.questNotified, false);
         conf.put(Key.zone_sync, "");
-        conf.put(Key.tree_q_show, "10");
+        conf.put(Key.tree_q_show, "17");
         conf.put(Key.lpassistent, false);
 
         ArrayList<HashMap<String, Object>> qpattern = new ArrayList<>();
